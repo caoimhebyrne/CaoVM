0271284809cb728bb47049709ae0b9f691692f76
Caoimhe <caoimhebyrne06@gmail.com>
2023-05-03 22:52:10 +0100

    ClassParser: Make `parse_attribute` public

--- src/ClassParser.h
+++ src/ClassParser.h
@@ -17,6 +17,7 @@ public:
     static ErrorOr<NonnullOwnPtr<ClassParser>> create(NonnullOwnPtr<Core::File> stream);
 
     ErrorOr<ClassFile> parse();
+    ErrorOr<NonnullOwnPtr<Attribute>> parse_attribute(NonnullOwnPtr<ConstantPool> const& constant_pool);
 
     // The JVM spec defines a few data types for unsigned integers, werid naming but sure...
     ErrorOr<u8> read_u1();
@@ -29,7 +30,6 @@ private:
     ErrorOr<ConstantClassInfo> parse_interface(NonnullOwnPtr<ConstantPool> const& constant_pool);
     ErrorOr<NonnullOwnPtr<FieldInfo>> parse_field(NonnullOwnPtr<ConstantPool> const& constant_pool);
     ErrorOr<NonnullOwnPtr<MethodInfo>> parse_method(NonnullOwnPtr<ConstantPool> const& constant_pool);
-    ErrorOr<NonnullOwnPtr<Attribute>> parse_attribute(NonnullOwnPtr<ConstantPool> const& constant_pool);
 
     NonnullOwnPtr<BigEndianInputBitStream> m_stream;
 };
