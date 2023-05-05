701adff3852b7f86863174367725c3d5a2dc0166
Caoimhe <caoimhebyrne06@gmail.com>
2023-05-05 23:07:13 +0100

    Interpreter: Use helper methods from `Parser::ConstantPool`

--- src/Interpreter/SymbolicatedReference.cpp
+++ src/Interpreter/SymbolicatedReference.cpp
@@ -26,26 +26,14 @@ SymbolicatedClassReference::SymbolicatedClassReference(u16 index, String name)
 
 ErrorOr<NonnullRefPtr<SymbolicatedClassReference>> SymbolicatedClassReference::create(u16 index, SymbolicatedConstantPool* symbolicated_pool)
 {
-    // The value of the `index` item must be a valid index into the constant_pool table.
-    auto const& potential_class_info = symbolicated_pool->parsed_pool()->entries().at(index - 1);
+    // The entry at the index must be a Constant_Class_info structure.
+    auto const& class_info = TRY(symbolicated_pool->parsed_pool()->class_at(index));
 
-    // The constant_pool entry at that index must be a CONSTANT_Class_info structure.
-    VERIFY(potential_class_info->tag() == Constant::Tag::Class);
-
-    // (Cast `potential_class_info` to the expected type)
-    auto& class_info = static_cast<Parser::ConstantClassInfo&>(*potential_class_info);
-
-    // The value of the `name_index` item must be a valid index into the constant_pool table.
-    auto const& potential_utf8_constant = symbolicated_pool->parsed_pool()->entries().at(class_info.name_index() - 1);
-
-    // The constant_pool entry at that index must be a CONSTANT_Utf8_info structure.
-    VERIFY(potential_utf8_constant->tag() == Constant::Tag::UTF8);
-
-    // (Cast `potential_class_info` to the expected type)
-    auto& name_utf8 = static_cast<Parser::ConstantUTF8Info&>(*potential_utf8_constant);
+    // The entry at `name_index` must be a Constant_Utf8_info structure.
+    auto const& name_utf8 = TRY(symbolicated_pool->parsed_pool()->utf8_at(class_info->name_index()));
 
     // For a nonarray class or an interface, the name is the binary name of the class or interface.
-    auto name = name_utf8.data();
+    auto name = name_utf8->data();
 
     // For an array class of n dimensions...
     // https://docs.oracle.com/javase/specs/jvms/se17/html/jvms-5.html#:~:text=For%20an%20array%20class%20of%20n%20dimensions
@@ -81,48 +69,24 @@ SymbolicatedMethodReference::SymbolicatedMethodReference(u16 index, String name,
 // Attempts to symbolicate a class reference, given its index into the parsed constant pool
 ErrorOr<NonnullRefPtr<SymbolicatedMethodReference>> SymbolicatedMethodReference::create(u16 index, SymbolicatedConstantPool* symbolicated_pool)
 {
-    // The value of the `index` item must be a valid index into the constant_pool table.
-    auto const& potential_method_info = symbolicated_pool->parsed_pool()->entries().at(index - 1);
+    // The entry at the index must be a Constant_Methodref_info structure.
+    auto const& method_info = TRY(symbolicated_pool->parsed_pool()->method_reference_at(index));
 
-    // The constant_pool entry at that index must be a CONSTANT_Methodref_info structure.
-    VERIFY(potential_method_info->tag() == Constant::Tag::MethodReference);
+    // The entry at `name_and_type_index` must be a CONSTANT_NameAndType_info structure.
+    auto const& name_and_type = TRY(symbolicated_pool->parsed_pool()->name_and_type_at(method_info->name_and_type_index()));
 
-    // (Cast `potential_method_info` to the expected type)
-    auto& method_info = static_cast<Parser::ConstantMethodReferenceInfo&>(*potential_method_info);
-
-    // The value of the `name_and_type_index` item must be a valid index into the constant_pool table.
-    auto const& potential_name_and_type_constant = symbolicated_pool->parsed_pool()->entries().at(method_info.name_and_type_index() - 1);
-
-    // The constant_pool entry at that index must be a CONSTANT_NameAndType_info structure.
-    VERIFY(potential_name_and_type_constant->tag() == Constant::Tag::NameAndType);
-
-    // (Cast `potential_name_and_type_constant` to the expected type)
-    auto& name_and_type = static_cast<Parser::ConstantNameAndTypeInfo&>(*potential_name_and_type_constant);
-
-    // The value of the `name_index` item must be a valid index into the constant_pool table.
-    auto const& potential_name_utf8_constant = symbolicated_pool->parsed_pool()->entries().at(name_and_type.name_index() - 1);
-
-    // The constant_pool entry at that index must be a CONSTANT_Utf8_info structure.
-    VERIFY(potential_name_utf8_constant->tag() == Constant::Tag::UTF8);
-    auto& name_utf8 = static_cast<Parser::ConstantUTF8Info&>(*potential_name_utf8_constant);
+    // The entries at `name_index` and `descriptor_index` must be CONSTANT_Utf8_info structures.
+    auto const& name_utf8 = TRY(symbolicated_pool->parsed_pool()->utf8_at(name_and_type->name_index()));
+    auto const& descriptor_utf8 = TRY(symbolicated_pool->parsed_pool()->utf8_at(name_and_type->descriptor_index()));
 
     // Represents either an unqualified name, or the special method name `<init>`
-    auto name = name_utf8.data();
-
-    // The value of the `name_index` item must be a valid index into the constant_pool table.
-    auto const& potential_descriptor_utf8_constant = symbolicated_pool->parsed_pool()->entries().at(name_and_type.descriptor_index() - 1);
-
-    // The constant_pool entry at that index must be a CONSTANT_Utf8_info structure.
-    VERIFY(potential_descriptor_utf8_constant->tag() == Constant::Tag::UTF8);
-
-    // (Cast `potential_descriptor_utf8_constant` to the expected type)
-    auto& descriptor_utf8 = static_cast<Parser::ConstantUTF8Info&>(*potential_descriptor_utf8_constant);
+    auto name = name_utf8->data();
 
     // Represents a valid field or method (in this case method) descriptor.
-    auto descriptor = descriptor_utf8.data();
+    auto descriptor = descriptor_utf8->data();
 
     // The value of the `class_index` type must correspond to a SymbolicatedClassReference.
-    auto owner = TRY(symbolicated_pool->get_or_symbolicate_class(method_info.class_index()));
+    auto owner = TRY(symbolicated_pool->get_or_symbolicate_class(method_info->class_index()));
 
     return try_make_ref_counted<SymbolicatedMethodReference>(index, name, descriptor, owner);
 }
